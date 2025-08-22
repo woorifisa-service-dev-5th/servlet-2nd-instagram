4f224131d05629f9d9290a7f6741716ff88609ca
kimingzero <lucy9150@naver.com>
2025-08-22 14:16:22 +0900

    [feat] comment-first

---
+++ Instagram/src/main/java/Dao/DBUtil.java
@@ -0,0 +1,21 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "1234";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    private static final String DATABASE_SCHEMA = "instagramdb";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(DB_URL + DATABASE_SCHEMA, USER_NAME, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
